38bcd01146bfdbfb7988b1715bffa330770f890c
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-06-05 18:30:52 +0630

    Add empty array and check books in local storage and add books

---
+++ script.js
@@ -0,0 +1,21 @@
+// Retrieve books from localStorage
+let books = [];
+
+// Check stored books in localStorage
+const storedBooks = localStorage.getItem('books');
+if (storedBooks) {
+  // If true, parse the JSON string into an array
+  books = JSON.parse(storedBooks);
+}
+
+// display books in the collection
+
+
+// add a new book to the collection
+
+
+//  remove a book from the collection
+
+// Event listener for form submission
+
+

be25aa072995a110a3d00fa3766d05bb0168e440
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-06-05 19:31:55 +0630

    Fix linters for CSS and Js files

--- script.js
+++ script.js
@@ -4,51 +4,51 @@ let books = [];
 // Check stored books in localStorage
 const storedBooks = localStorage.getItem('books');
 if (storedBooks) {
-    // If true, parse the JSON string into an array
-    books = JSON.parse(storedBooks);
+  // If true, parse the JSON string into an array
+  books = JSON.parse(storedBooks);
 }
 
 // display books in the collection
 function displayBooks() {
-    const booksList = document.getElementById('books-list');
-    booksList.innerHTML = '';
-    books.forEach((book, index) => {
-        const bookDiv = document.createElement('div');
-        bookDiv.innerHTML = `<div>${book.title}</div>
+  const booksList = document.getElementById('books-list');
+  booksList.innerHTML = '';
+  books.forEach((book, index) => {
+    const bookDiv = document.createElement('div');
+    bookDiv.innerHTML = `<div>${book.title}</div>
                         <div>${book.author}</div>
                          <button onclick="removeBook(${index})">Remove</button>
                          <hr>`;
-        booksList.appendChild(bookDiv);
-    });
+    booksList.appendChild(bookDiv);
+  });
 }
 
 // add a new book to the collection
 function addBook(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const titleInput = document.getElementById('title-input');
-    const authorInput = document.getElementById('author-input');
-    const newBook = {
-        title: titleInput.value,
-        author: authorInput.value,
-    };
+  const titleInput = document.getElementById('title-input');
+  const authorInput = document.getElementById('author-input');
+  const newBook = {
+    title: titleInput.value,
+    author: authorInput.value,
+  };
 
-    books.push(newBook);
-    localStorage.setItem('books', JSON.stringify(books));
+  books.push(newBook);
+  localStorage.setItem('books', JSON.stringify(books));
 
-    displayBooks();
+  displayBooks();
 
-    // Reset input fields
-    titleInput.value = '';
-    authorInput.value = '';
+  // Reset input fields
+  titleInput.value = '';
+  authorInput.value = '';
 }
 
 //  remove a book from the collection
 function removeBook(index) {
-    books = books.filter((book, bookIndex) => bookIndex !== index);
-    localStorage.setItem('books', JSON.stringify(books));
+  books = books.filter((book, bookIndex) => bookIndex !== index);
+  localStorage.setItem('books', JSON.stringify(books));
 
-    displayBooks();
+  displayBooks();
 }
 
 // Event listener for form submission
